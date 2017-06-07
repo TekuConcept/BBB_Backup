d9a39af2744b0f598256c1acfcd0e10cbe542242
TekuConcept <electro_madness@yahoo.com>
2017-06-07 15:59:25 -0600

    Added C++ Test

---
+++ ReactNativeDjinni/cpp_project/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "hello_world_impl.hpp"
+
+int main(int argc, const char * argv[]) {
+    helloworld::HelloWorldImpl hw = helloworld::HelloWorldImpl();
+    std::string myString = hw.get_hello_world();
+    std::cout << myString << "\n";
+    return 0;
+}
